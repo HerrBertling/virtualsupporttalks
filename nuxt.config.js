660bef47d2ed730743e58aa8f303aed735c914d4
Markus Siering <markus277@gmail.com>
2021-03-15 21:04:47 +0100

    fix(favicon): remove telephone emoji from nuxt.config
    okay, now this was utterly stupid.
    
    closes #6

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,14 +27,6 @@ export default {
         content: process.env.npm_package_description || '',
       },
     ],
-    link: [
-      {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href:
-          'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☎️</text></svg>',
-      },
-    ],
   },
   router: {
     trailingSlash: true,
@@ -80,9 +72,7 @@ export default {
    */
   buildModules: [
     'nuxt-esbuild',
-    // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
     '@nuxtjs/stylelint-module',
     '@nuxtjs/google-analytics',
   ],
