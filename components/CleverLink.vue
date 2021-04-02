6d00635764bcb4f44691d5f3335c96faf1313a44
Markus Siering <markus277@gmail.com>
2021-04-02 14:02:56 +0200

    feat(links): resolve internal links from CMS correctly
    closes #7

---
+++ components/CleverLink.vue
@@ -0,0 +1,47 @@
+<template>
+  <component :is="linkComponent" v-bind="linkAttrs">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'CleverLink',
+
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    linkComponent() {
+      if (this.isInternalLink) {
+        return 'NuxtLink'
+      }
+      return 'a'
+    },
+
+    isInternalLink() {
+      return this.to.startsWith('https://www.virtualsupporttalks.de/')
+    },
+
+    linkAttrs() {
+      let usedLink = this.to
+      if (this.isInternalLink) {
+        usedLink = this.to.replace('https://www.virtualsupporttalks.de', '')
+        return {
+          ...this.$attrs,
+          to: this.localePath(usedLink),
+        }
+      }
+      return {
+        ...this.$attrs,
+        href: usedLink,
+        rel: 'noopener',
+      }
+    },
+  },
+}
+</script>
