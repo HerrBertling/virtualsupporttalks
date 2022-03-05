3b007b00dcc6b52d05cf2f1090bddf3162167379
Markus Siering <markus277@gmail.com>
2022-03-05 16:57:46 +0100

    fix(navigation): use exact active for nav item active styles

--- components/layout/NavItem.vue
+++ components/layout/NavItem.vue
@@ -2,7 +2,7 @@
   <li class="w-full block lg:w-auto lg:mr-1" @click="$emit('click')">
     <nuxt-link
       class="block p-4 no-underline lg:inline-block lg:py-1 lg:px-2 lg:rounded-md"
-      active-class="bg-gray-100 lg:bg-white border-l-4 border-l-vsp-500 lg:border-none"
+      exact-active-class="bg-gray-100 lg:bg-white border-l-4 border-b-transparent border-l-vsp-500 lg:border-l-transparent lg:underline lg:decoration-vsp-500"
       :to="to"
     >
       <slot />
