c09d8ec53332c7c1a00d1cd8dc46422542c8c4c4
Markus Siering <markus277@gmail.com>
2022-03-13 20:44:34 +0100

    chore: remove everything from nuxt

--- plugins/contentful.js
+++
@@ -1,8 +0,0 @@
-const contentful = require('contentful')
-export default (context, inject) => {
-  const cms = contentful.createClient({
-    space: context.$config.contentfulSpace,
-    accessToken: context.$config.contentfulKey,
-  })
-  inject('contentful', cms)
-}
--- plugins/cookie-law.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import CookieLaw from 'vue-cookie-law'
-
-export default () => {
-  Vue.component('CookieLaw', CookieLaw)
-}
