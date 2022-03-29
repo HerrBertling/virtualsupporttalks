b519740d06974ce8c39a0e9a9b0dc170d4a5c06f
Markus Siering <markus277@gmail.com>
2022-03-29 14:27:54 +0200

    chore(content-blocks): put image on top of text for fullsize img block

--- app/components/ContentBlocks/FullSizeImageBg.tsx
+++ app/components/ContentBlocks/FullSizeImageBg.tsx
@@ -20,13 +20,13 @@ export default function FullSizeImageBg({
         <img
           src={image.fields.file.url}
           className={`h-auto max-h-[60vh] w-full object-cover lg:h-full lg:max-h-max ${
-            imageRight && "lg:col-start-2 lg:row-start-1"
+            imageRight && "row-start-1 lg:col-start-2"
           }`}
         />
       )}
       <div
         className={`py-8 px-4 lg:max-w-[70ch] lg:px-12 lg:pb-12 lg:pt-36 ${
-          imageRight && "col-start-1 row-start-1 ml-auto"
+          imageRight && "col-start-1 ml-auto lg:row-start-1"
         }`}
       >
         {children}
