7207e4b5e2b4501aa3729bbc4f4224801028c837
Markus Siering <markus277@gmail.com>
2022-04-14 20:00:28 +0200

    feat: add powered by contentful badge to footer

--- components/layout/LayoutFooter.vue
+++ components/layout/LayoutFooter.vue
@@ -10,6 +10,18 @@
         </nuxt-link>
       </li>
     </ul>
+    <a
+      class="block my-8"
+      href="https://www.contentful.com/"
+      rel="nofollow"
+      target="_blank"
+    >
+      <img
+        src="https://images.ctfassets.net/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg"
+        class="max-w-[100px] mx-auto w-full"
+        alt="Powered by Contentful"
+      />
+    </a>
     <client-only>
       <cookie-law theme="dark-lime">
         <div
