3e46a1c9083e23160e395b1dd8be1d120c645953
NoeliaEle <noelia.leon@project-a.com>
2022-07-30 12:56:09 +0200

    adjust UI

--- app/components/ContentBlocks/VideoPlayer.tsx
+++ app/components/ContentBlocks/VideoPlayer.tsx
@@ -2,11 +2,11 @@ import ContentfulRichText from "../ContentfulRichText";
 
 export default function VideoPlayer({ videoId, text, content }) {
   return (
-    <section className="grid w-screen max-w-full grid-cols-1 lg:grid-cols-2">
-      <div className="col-start-1 py-8 px-4 lg:row-start-1 lg:ml-auto lg:max-w-[70ch] lg:px-12 lg:pb-12 lg:pt-36">
+    <section className="my-1 grid w-screen max-w-full grid-cols-1 lg:grid-cols-2">
+      <div className=" flex-grid self-center py-8 px-4 lg:row-start-1 lg:ml-auto lg:max-w-[70ch] lg:px-12 lg:pt-24 lg:pb-12">
         {content && <ContentfulRichText content={content} />}
       </div>
-      <div className="row-start-2 h-auto max-h-[60vh] w-full lg:col-start-2 lg:row-start-1 lg:max-h-max">
+      <div className="row-start-2 h-auto max-h-[60vh] w-full lg:col-start-2 lg:row-start-1 lg:max-h-max lg:p-4">
         <iframe
           width="100%"
           height="100%"
