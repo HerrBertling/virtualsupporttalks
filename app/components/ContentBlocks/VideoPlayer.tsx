54d6eaae9ecc9807915bff06975311d384511cdd
Francine Melo Pêpe <franmelopepe2@gmail.com>
2022-08-12 13:40:11 +0200

    Aspect ratio 16/9 defined to the Video component (#117)

--- app/components/ContentBlocks/VideoPlayer.tsx
+++ app/components/ContentBlocks/VideoPlayer.tsx
@@ -7,7 +7,7 @@ export default function VideoPlayer({ videoId, content }: IVideoPlayerFields) {
       <div className=" flex-grid self-center py-8 px-4 lg:row-start-1 lg:ml-auto lg:max-w-[70ch] lg:px-12 lg:pt-24 lg:pb-12">
         {content && <ContentfulRichText content={content} />}
       </div>
-      <div className="row-start-2 h-auto max-h-[60vh] w-full lg:col-start-2 lg:row-start-1 lg:max-h-max lg:p-4">
+      <div className="row-start-2 h-auto w-full lg:col-start-2 lg:row-start-1 lg:max-h-max lg:p-4">
         <iframe
           width="100%"
           height="100%"
@@ -16,7 +16,9 @@ export default function VideoPlayer({ videoId, content }: IVideoPlayerFields) {
           allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
           style={{
-            height: "400px",
+            width: "100%",
+            aspectRatio: "16/9",
+            objectFit: "cover",
           }}
         />
       </div>
