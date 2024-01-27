a6438bb17cd5448c2f8188fc09fdd72930318534
NoeliaEle <88032788+NoeliaEle@users.noreply.github.com>
2024-01-27 20:38:28 +0100

    fix component UI and types

--- app/components/ContentBlocks/BlogPreview.tsx
+++ app/components/ContentBlocks/BlogPreview.tsx
@@ -1,25 +1,30 @@
-import { useTranslation } from "react-i18next";
 import BasicCatchBoundary from "~/components/BasicErrorBoundary";
 import BlogpostCard from "~/components/BlogpostCard";
 import { IBlogpost, LOCALE_CODE } from "@types/generated/contentful";
 import { useLoaderData } from "@remix-run/react";
 import CleverButton from "../CleverButton";
+import ContentfulRichText from "../ContentfulRichText";
+import type { Document } from "@contentful/rich-text-types";
 
-export default function ContentBlockBlogPreview() {
+interface ContentBlockBlogPreviewProps {
+  titleAndHeader: Document;
+  buttonText: string;
+}
+
+export default function ContentBlockBlogPreview({
+  titleAndHeader,
+  buttonText,
+}: ContentBlockBlogPreviewProps) {
   const { latestPosts, locale } = useLoaderData<{
     latestPosts: IBlogpost[];
     locale: LOCALE_CODE;
   }>();
 
-  const { t } = useTranslation("blogpostOverview");
-
   return (
     latestPosts && (
-      <>
-        <header className="w-full px-4 pt-32 pb-12">
-          <h2 className="font-headline text-3xl font-bold">
-            {t("contentBlockTitle")}
-          </h2>
+      <div className="w-full px-4 pt-24 mx-auto max-w-7xl">
+        <header>
+          <ContentfulRichText content={titleAndHeader} />
         </header>
         <div className="my-4 mx-auto grid grid-cols-1 gap-16 px-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 lg:gap-y-16 lg:gap-x-12">
           {latestPosts.map((post: IBlogpost) => (
@@ -31,10 +36,10 @@ export default function ContentBlockBlogPreview() {
             />
           ))}
         </div>
-        <div className="w-full text-center">
-          <CleverButton to={`blog`}> {t("contentBlockButton")}</CleverButton>
+        <div className="w-full text-center mt-8">
+          <CleverButton to={"blog"}> {buttonText}</CleverButton>
         </div>
-      </>
+      </div>
     )
   );
 }
