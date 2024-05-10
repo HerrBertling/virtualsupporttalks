8ceacb96102880f5ed65576d4427a1ffeb2c346a
NoeliaEle <88032788+NoeliaEle@users.noreply.github.com>
2024-05-10 21:12:03 +0200

    adjust color of banner

--- app/components/ContentBlocks/Banner.tsx
+++ app/components/ContentBlocks/Banner.tsx
@@ -3,7 +3,7 @@ import ContentfulRichText from "../ContentfulRichText";
 
 export default function Banner({ textInBanner, action }: IBannerFields) {
   return (
-    <section className="flex items-center justify-center fixed bottom-0 w-full py-2 px-4 bg-gradient-to-b from-vsp-600 to-vsp-300 z-10 text-center">
+    <section className="flex items-center justify-center fixed bottom-0 w-full py-2 px-4 bg-gradient-to-b to-orange-400 from-orange-500 shadow-[rgba(0,0,15,0.5)_5px_5px_5px_6px] z-10 text-center">
       <a href={action}>
         <ContentfulRichText content={textInBanner}></ContentfulRichText>
       </a>
