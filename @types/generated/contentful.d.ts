bbe5fbe1530c5131c3a11437b9cd3a20745a234c
Francine Melo Pêpe <franmelopepe2@gmail.com>
2024-07-01 12:02:39 +0200

    TestimonialSlider (#268)
    * contentful types for testimonials section
    
    * testimonials content blocks
    
    * testimonial component to display the partners/users testimonials about Redezeit
    
    * testimonial TITLE. It´s separated from the main component, because Contentful was not displaying the testimonials array.
    
    * contentful client - testimonials

--- @types/generated/contentful.d.ts
+++ @types/generated/contentful.d.ts
@@ -634,6 +634,8 @@ export interface IPageFields {
     | ITrackingGa
     | IBlogPreview
     | ITeamSection
+    | ITestimonialSection
+    | ITestimonials
   )[];
 
   /** seo */
@@ -776,6 +778,64 @@ export interface ITeamSection extends Entry<ITeamSectionFields> {
   };
 }
 
+export interface ITestimonialsFields {
+  /** image */
+  image?: Asset | undefined;
+
+  /** link */
+  link?: string | undefined;
+
+  /** testimonialText */
+  testimonialText?: string | undefined;
+
+  /** author */
+  author?: string | undefined;
+}
+
+export interface ITestimonials extends Entry<ITestimonialsFields> {
+  sys: {
+    id: string;
+    type: string;
+    createdAt: string;
+    updatedAt: string;
+    locale: string;
+    contentType: {
+      sys: {
+        id: "testimonials";
+        linkType: "ContentType";
+        type: "Link";
+      };
+    };
+  };
+}
+
+export interface ITestimonialSectionFields {
+  /** Title */
+  title: string;
+
+  /** Testimonials */
+  testimonials: ITestimonials[];
+}
+
+/** Here you can find the testimonials grouped */
+
+export interface ITestimonialSection extends Entry<ITestimonialSectionFields> {
+  sys: {
+    id: string;
+    type: string;
+    createdAt: string;
+    updatedAt: string;
+    locale: string;
+    contentType: {
+      sys: {
+        id: "testimonialSection";
+        linkType: "ContentType";
+        type: "Link";
+      };
+    };
+  };
+}
+
 export interface ITrackingGaFields {
   /** Title */
   title: string;
@@ -899,6 +959,8 @@ export type CONTENT_TYPE =
   | "supporter"
   | "tag"
   | "teamSection"
+  | "testimonials"
+  | "testimonialSection"
   | "trackingGa"
   | "twoImages"
   | "videoPlayer";
@@ -927,6 +989,8 @@ export type IEntry =
   | ISupporter
   | ITag
   | ITeamSection
+  | ITestimonials
+  | ITestimonialSection
   | ITrackingGa
   | ITwoImages
   | IVideoPlayer;
---
+++ app/components/ContentBlocks/Testimonials.tsx
@@ -0,0 +1,11 @@
+import { type ITestimonialSectionFields } from "../../../@types/generated/contentful";
+
+function Testimonials({ title }: ITestimonialSectionFields) {
+  return (
+    <section className="mx-auto max-w-7xl px-4 pt-12 md:px-12">
+      <h2 className="mb-12 font-headline text-4xl font-bold">{title}</h2>
+    </section>
+  );
+}
+
+export default Testimonials;
