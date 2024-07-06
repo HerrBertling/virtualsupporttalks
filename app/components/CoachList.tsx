47d7e8af0458749629a84c731c6c151f6332262e
Sarah <sarahmatthies93@gmail.com>
2024-07-06 14:44:35 +0200

    add faux masonry layout

--- app/components/CoachList.tsx
+++ app/components/CoachList.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 export default function CoachList({ children }: { children: ReactNode }) {
   return (
     <div className="bg-slate-100">
-      <section className="mx-auto grid max-w-7xl grid-cols-coachgrid items-start gap-x-6 gap-y-12 py-12 px-4">
+      <section className="mx-auto block max-w-7xl gap-x-6 py-12 px-4 columns-sm">
         {children}
       </section>
     </div>
