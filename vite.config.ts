36ab83ab8ce9cf1ca743478bfa9104839820ef50
Sarah <sarahmatthies93@gmail.com>
2024-09-19 20:01:54 +0200

    add netlify plugin without edge functions

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { netlifyPlugin } from "@netlify/remix-edge-adapter/plugin";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 import { remixDevTools } from "remix-development-tools";
 
 
 export default defineConfig({
   plugins: [remixDevTools(), remix({
   ignoredRouteFiles: ["**/.*"],
-  }), tsconfigPaths()],
+  }), netlifyPlugin(), tsconfigPaths()],
 });
