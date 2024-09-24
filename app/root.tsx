8ebe78fb3c5c5ae4bd35d6d137e62e2b21c2698d
Markus Siering <markus.siering@zolar.de>
2024-09-24 21:58:19 +0200

    refactor: use Layout component in root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,8 +1,11 @@
-import type { LinksFunction, LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import type {
+  LinksFunction,
+  LoaderFunctionArgs,
+  MetaFunction,
+} from "@remix-run/node";
 import { json } from "@remix-run/node";
 import {
   Links,
-  LiveReload,
   Meta,
   Outlet,
   Scripts,
@@ -13,7 +16,7 @@ import { useTranslation } from "react-i18next";
 import * as gtag from "~/utils/gtag.client";
 import { getSeo } from "~/seo";
 import { gdprConsent } from "./cookies";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import BasicCatchBoundary from "./components/BasicErrorBoundary";
 import styles from "./styles/app.css?url";
 import { CookieBanner } from "./components/CookieBanner";
@@ -23,9 +26,11 @@ let [seoMeta, seoLinks] = getSeo();
 const GA_TRACKING_ID = "GTM-NH6W3MZ";
 
 export const meta: MetaFunction = () => {
-  return [{
-    ...seoMeta,
-  }];
+  return [
+    {
+      ...seoMeta,
+    },
+  ];
 };
 
 export const links: LinksFunction = () => {
@@ -36,8 +41,33 @@ export const links: LinksFunction = () => {
   ];
 };
 
+export const Layout = ({
+  children,
+  locale,
+}: {
+  children: ReactNode;
+  locale: string;
+}) => {
+  let { i18n } = useTranslation();
+  return (
+    <html lang={locale} dir={i18n.dir()}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+};
+
 export async function loader({ request, params }: LoaderFunctionArgs) {
-  let { locale = 'de' } = params;
+  let { locale = "de" } = params;
   const cookieHeader = request.headers.get("Cookie");
   const cookie = (await gdprConsent.parse(cookieHeader)) || {};
   return json({ locale, track: cookie.gdprConsent });
@@ -53,7 +83,6 @@ export function useChangeLanguage(locale: string) {
 export default function App() {
   let { locale, track } = useLoaderData<typeof loader>();
   const [shouldTrack, setShouldTrack] = useState(track);
-  let { i18n } = useTranslation();
 
   useEffect(() => {
     setShouldTrack(track);
@@ -68,52 +97,25 @@ export default function App() {
   useChangeLanguage(locale);
 
   return (
-    <html lang={locale} dir={i18n.dir()}>
-      <head>
-      
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
-        {shouldTrack && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <>
+      {shouldTrack && (
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','${GA_TRACKING_ID}');`,
-            }}
-          />
-        )}
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {!shouldTrack && <CookieBanner initialOpen={!shouldTrack} />}
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
+          }}
+        />
+      )}
+
+      {!shouldTrack && <CookieBanner initialOpen={!shouldTrack} />}
+      <Outlet />
+    </>
   );
 }
 
 export function ErrorBoundary() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <BasicCatchBoundary />
-        ;
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+  return <BasicCatchBoundary />;
 }
