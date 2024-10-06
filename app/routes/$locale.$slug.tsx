2bebd10608a3712a1ad01259e8d93b45de8e2dc7
Markus Siering <markus.siering@zolar.de>
2024-10-06 15:52:49 +0200

    feat: add caching according to Netlify caching article
    as seen here: https://developers.netlify.com/guides/how-to-do-isr-and-advanced-caching-with-remix/\#optimizing-durable-and-stale-while-revalidate
    
    closes #274

--- app/routes/$locale.$slug.tsx
+++ app/routes/$locale.$slug.tsx
@@ -1,4 +1,4 @@
-import type { LoaderFunction, MetaFunction } from "@remix-run/node";
+import type { HeadersFunction, LoaderFunction, MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import BasicCatchBoundary from "~/components/BasicErrorBoundary";
 import ContentBlocks from "~/components/ContentBlocks";
@@ -30,6 +30,13 @@ export const meta: MetaFunction = ({ data }) => {
     },
   ];
 };
+
+export const headers: HeadersFunction = () => ({
+  // Tell the browser to always check the freshness of the cache
+  "Cache-Control": "public, max-age=0, must-revalidate",
+  // Tell the CDN to treat it as fresh for 5 minutes, but for a day after that return a stale version while it revalidates
+  "Netlify-CDN-Cache-Control": "public, durable, s-maxage=300, stale-while-revalidate=86400",
+});
 
 export const loader: LoaderFunction = async ({ params }) => {
   const { slug, locale } = params;
--- app/routes/de.netzwerk-partner-medien.tsx
+++ app/routes/de.netzwerk-partner-medien.tsx
@@ -1,4 +1,4 @@
-import type { LoaderFunction, MetaFunction } from "@remix-run/node";
+import type { HeadersFunction, LoaderFunction, MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import NetworkPartnerMediaContent from "~/components/NetworkPartnerMediaContent";
 import { getSeoMeta } from "~/seo";
@@ -10,6 +10,13 @@ import {
   getSupporters,
 } from "~/utils/contentful";
 import pageIds from "~/utils/pageIds";
+
+export const headers: HeadersFunction = () => ({
+  // Tell the browser to always check the freshness of the cache
+  "Cache-Control": "public, max-age=0, must-revalidate",
+  // Tell the CDN to treat it as fresh for 5 minutes, but for a day after that return a stale version while it revalidates
+  "Netlify-CDN-Cache-Control": "public, durable, s-maxage=300, stale-while-revalidate=86400",
+});
 
 export const meta: MetaFunction = ({ data }) => {
   const { title, seo } = data?.page?.fields;
