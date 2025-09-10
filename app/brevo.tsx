3444f7d537c6277175ba925bc852ec86eb9b3c0b
Sarah <sarahmatthies93@gmail.com>
2025-09-10 23:36:35 +0200

    insert Brevo into the head

--- app/brevo.tsx
+++ app/brevo.tsx
@@ -1,11 +1,19 @@
+import { useEffect } from "react";
+
 const Brevo = () => {
+useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.async = true;
+    script.id = "brevo-script";
+    script.src = "https://cdn.brevo.com/js/sdk-loader.js";
+    document.head.appendChild(script);
+  }, []);
 return (
-<><script src="https://cdn.brevo.com/js/sdk-loader.js" async></script>
 <script dangerouslySetInnerHTML={{
     __html: `window.Brevo = window.Brevo || []; Brevo.push(["init",{client_key: "ir90lm6djp5yl513z778118q"}]);`
 }}/>
-    
-</>)
-}
+)
+};
 
 export default Brevo;
